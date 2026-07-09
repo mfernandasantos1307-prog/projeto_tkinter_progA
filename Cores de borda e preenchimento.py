942bd95e7927753a6e19767c526b9c20cb848316
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-07-09 16:33:10 -0300

    Update Cores de borda e preenchimento.py

--- Cores de borda e preenchimento.py
+++ Cores de borda e preenchimento.py
@@ -6,8 +6,16 @@ cor_preenchimento_atual = 'white'
 
 def escolher_cor_preenchimento():
   global cor_preenchimento_atual
-  cor = colorchooser.askcolor(Title='Escolha a cor de preenchimento')
+  cor = colorchooser.askcolor(title='Escolha a cor de preenchimento')
   if cor[1]:
     cor_preenchimento_atual = cor[1]
+  return cor_preenchimento_atual
 
 # Função para borda
+
+def escolher_cor_borda():
+  global cor_borda_atual
+  cor = colorchooser.askcolor(title='Escolha a cor da borda')
+  if cor[1]:
+    cor_borda_atual = cor[1]
+  return cor_borda_atual
