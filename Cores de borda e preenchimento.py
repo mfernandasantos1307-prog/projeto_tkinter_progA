814b25a44846a4a47f621933062b327d77e55ff2
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-07-09 16:27:58 -0300

    Implement color chooser for fill color selection
    Added a color chooser for selecting fill color.

--- Cores de borda e preenchimento.py
+++ Cores de borda e preenchimento.py
@@ -1,3 +1,13 @@
 # Função para preenchimento
+from tkinter import colorchooser
+
+cor_borda_atual = 'black'
+cor_preenchimento_atual = 'white'
+
+def escolher_cor_preenchimento():
+  global cor_preenchimento_atual
+  cor = colorchooser.askcolor(Title='Escolha a cor de preenchimento')
+  if cor[1]:
+    cor_preenchimento_atual = cor[1]
 
 # Função para borda
