3a7481d6e1dd7f03f5b946dbe158f530ce000733
Eloisa Zifirino <eloisazifirino098@gmail.com>
2026-07-15 19:25:34 -0300

    Implementa controle de retângulos e ovais

--- src/app_desenhos/controlador/editor_controller.py
+++ src/app_desenhos/controlador/editor_controller.py
@@ -29,14 +29,26 @@ class EditorController:
     def iniciar_figura(self, event):
         if self.ferramenta_atual == "circulo":
             self.iniciar_circulo(event)
+        elif self.ferramenta_atual == "retangulo":
+            self.iniciar_retangulo(event)
+        elif self.ferramenta_atual == "oval":
+            self.iniciar_oval(event)
 
     def atualizar_figura(self, event):
         if self.ferramenta_atual == "circulo":
             self.atualizar_circulo(event)
+        elif self.ferramenta_atual == "retangulo":
+            self.atualizar_retangulo(event)
+        elif self.ferramenta_atual == "oval":
+            self.atualizar_oval(event)
 
     def finalizar_figura(self, event):
         if self.ferramenta_atual == "circulo":
             self.finalizar_circulo(event)
+        elif self.ferramenta_atual == "retangulo":
+            self.finalizar_retangulo(event)
+        elif self.ferramenta_atual == "oval":
+            self.finalizar_oval(event)
 
     def finalizar_poligono(self, event):
         pass
@@ -78,3 +90,65 @@ class EditorController:
             (self.ini_x - fim_x) ** 2
             + (self.ini_y - fim_y) ** 2
         ) ** 0.5
+
+    # ==========================================
+    # RETÂNGULO
+    # ==========================================
+
+    def iniciar_retangulo(self, event):
+        self.ini_x = event.x
+        self.ini_y = event.y
+
+    def atualizar_retangulo(self, event):
+        self.fim_x = event.x
+        self.fim_y = event.y
+
+        retangulo = self.criar_retangulo(self.fim_x, self.fim_y)
+        self.view.redesenhar(self.desenho.obter_figuras())
+        self.view.desenhar_previa(retangulo)
+
+    def finalizar_retangulo(self, event):
+        retangulo = self.criar_retangulo(event.x, event.y)
+        self.desenho.adicionar_figura(retangulo)
+        self.view.redesenhar(self.desenho.obter_figuras())
+
+    def criar_retangulo(self, fim_x, fim_y):
+        return Retangulo(
+            self.view.obter_cor_borda(),
+            self.view.obter_cor_preenchimento(),
+            self.ini_x,
+            self.ini_y,
+            fim_x,
+            fim_y
+        )
+
+    # ==========================================
+    # OVAL
+    # ==========================================
+
+    def iniciar_oval(self, event):
+        self.ini_x = event.x
+        self.ini_y = event.y
+
+    def atualizar_oval(self, event):
+        self.fim_x = event.x
+        self.fim_y = event.y
+
+        oval = self.criar_oval(self.fim_x, self.fim_y)
+        self.view.redesenhar(self.desenho.obter_figuras())
+        self.view.desenhar_previa(oval)
+
+    def finalizar_oval(self, event):
+        oval = self.criar_oval(event.x, event.y)
+        self.desenho.adicionar_figura(oval)
+        self.view.redesenhar(self.desenho.obter_figuras())
+
+    def criar_oval(self, fim_x, fim_y):
+        return Oval(
+            self.view.obter_cor_borda(),
+            self.view.obter_cor_preenchimento(),
+            self.ini_x,
+            self.ini_y,
+            fim_x,
+            fim_y
+        )
