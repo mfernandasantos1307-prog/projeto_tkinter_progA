a1f699a7ea481fa34e60e42e9626ff7dc3eed9d6
Eloisa Zifirino <eloisazifirino098@gmail.com>
2026-07-15 19:41:20 -0300

    Transforma Figura em classe abstrata

--- src/app_desenhos/model/figuras.py
+++ src/app_desenhos/model/figuras.py
@@ -1,9 +1,13 @@
-class Figura:
+from abc import ABC, abstractmethod
+
+
+class Figura(ABC):
     
     def __init__(self, cor_borda, cor_preenchimento):
         self.cor_borda = cor_borda
         self.cor_preenchimento = cor_preenchimento
     
+    @abstractmethod
     def desenhar(self, canvas):
         pass
 
