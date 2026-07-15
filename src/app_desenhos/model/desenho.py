f7debf417f3a8195cdc3b01f3b9a01399cd10626
Maria Luíse das Virgens Menezes <marialuise02@gmail.com>
2026-07-15 14:40:16 -0300

    Implement Desenho class for managing figures

---
+++ src/app_desenhos/model/desenho.py
@@ -0,0 +1,23 @@
+from model.figuras import Figura
+
+class Desenho:
+    
+    def __init__(self):
+        self._figuras = []
+
+    def adicionar_figura(self, figura: Figura):
+        self._figuras.append(figura)
+
+    def remover_figura(self, figura: Figura):
+        if figura in self._figuras:
+            self._figuras.remove(figura)
+
+    def limpar(self):
+        self._figuras.clear()
+
+    def obter_figuras(self):
+        return self._figuras
+
+    def desenhar_todos(self, canvas):
+        for figura in self._figuras:
+            figura.desenhar(canvas)
